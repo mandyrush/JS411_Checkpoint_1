efb114f7672bccfb48d9a3f3cdf1543784ab8046
mandyrush <mandyrush85@gmail.com>
2021-11-14 13:46:57 -0700

    create navbar and login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              My Music App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        className="login-form"
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="standard-basic" label="Username *" size="small" variant="standard" />
+        <TextField id="standard-basic" label="Password *" size="small" variant="standard" />
+        <Button variant="contained">Login</Button>
+      </Box>
     </div>
   );
 }
